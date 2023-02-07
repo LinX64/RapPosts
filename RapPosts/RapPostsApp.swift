3e5728ea28ab1604a99403f382f66d6150f38e27
Mohsen Rzna <ash.wxrz@hotmail.com>
2023-02-07 21:43:55 +0100

    Initial Commit

---
+++ RapPosts/RapPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapPostsApp.swift
+//  RapPosts
+//
+//  Created by Mohsen Rezania on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RapPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
