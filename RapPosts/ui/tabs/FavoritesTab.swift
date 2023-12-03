6f81b2e729c9daad91aa4d3c9b0f9b8373a5d47b
Mohsen Rzna <ash.wxrz@hotmail.com>
2023-12-03 05:35:51 +0100

    Added directory for tabs + fixed typo

---
+++ RapPosts/ui/tabs/FavoritesTab.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct FavoritesTab: View {
+    var body: some View {
+        Text("Favorites")
+    }
+}
+
+struct FavoritesTab_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesTab()
+    }
+}
